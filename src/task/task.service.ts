c6f1f14345459f590e200128e0491cc700cddf2b
Darío José Zamora Vargas <dajozavargas@gmail.com>
2024-12-24 17:06:31 -0500

    add logic service task

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,4 +1,48 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { Task } from '@prisma/client';
 
 @Injectable()
-export class TaskService {}
+export class TaskService {
+  constructor(private prisma: PrismaService) {}
+
+  // list all tasks
+  async getAllTasks(): Promise<Task[]> {
+    return this.prisma.task.findMany();
+  }
+
+  // get a task by id
+  async getTaskById(id: number): Promise<Task> {
+    return this.prisma.task.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
+
+  // create a task
+  async createTask(data: Task): Promise<Task> {
+    return this.prisma.task.create({
+      data,
+    });
+  }
+
+  //   update a task
+  async updateTask(id: number, data: Task): Promise<Task> {
+    return this.prisma.task.update({
+      where: {
+        id,
+      },
+      data,
+    });
+  }
+
+  //   delete a task
+  async deleteTask(id: number): Promise<Task> {
+    return this.prisma.task.delete({
+      where: {
+        id,
+      },
+    });
+  }
+}
